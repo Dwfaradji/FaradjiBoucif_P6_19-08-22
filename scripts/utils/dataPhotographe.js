1c02d8edd6e8b4b5b22b29f59ee5e5832e2f4dfc
Dwfaradji <85832124+Dwfaradji@users.noreply.github.com>
2022-09-21 19:01:05 +0200

    version final

--- scripts/utils/dataPhotographe.js
+++ scripts/utils/dataPhotographe.js
@@ -1,6 +1,6 @@
 export default async function data() {
     try {
-        const response = await fetch('../../data/photographers.json');
+        const response = await fetch("../data/photographers.json");
         const promise = await response.json();
         return promise;
     } catch (error) {

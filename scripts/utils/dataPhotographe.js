be6f95168499a0c1465072878f4072eb0f0af1b5
Dwfaradji <85832124+Dwfaradji@users.noreply.github.com>
2022-09-22 15:30:44 +0200

    mise en ligne git page

--- scripts/utils/dataPhotographe.js
+++ scripts/utils/dataPhotographe.js
@@ -1,10 +1,5 @@
-
 export default async function data() {
-    try {
-        const response = await fetch("../../data/photographers.json");
-        const promise = await response.json();
-        return promise;
-    } catch (error) {
-        console.log(error);
-    }
+    const response = await fetch("../../data/photographers.json");
+    const promise = await response.json();
+    return promise;
 }
